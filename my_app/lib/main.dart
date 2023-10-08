eeb71fd03e157478cb38446f32ecd6e4d8fce56c
Flutter <rabunthoeun7777@gmail.com>
2023-10-08 14:46:25 +0700

    first commit

---
+++ my_app/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/page/HomePage.dart';
+
+void main() {
+  // ignore: prefer_const_constructors
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: const HomePage(),
+  ));
+}
+
---
+++ my_app/lib/page/HomePage.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("THOEUN"),
+      ),
+      drawer: Drawer(
+        child:  ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text("CST"),
+              ),
+              ListTile(
+                title: const Text("Home"),
+                onTap: () {
+                  
+                },
+              ),
+              ListTile(
+                title: const Text("About"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Cars"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Model"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text("Setting"),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              )
+          ],
+        ),
+      ),
+
+      body: 
+      
+      const Text("data",style: TextStyle(fontSize: 20,),),
+    
+    );
+  }
+}
